a0f9b2aff1914a5e37580d39db7f6062b5c47a4f
ElainW <wangyilan0304@gmail.com>
2026-03-23 00:10:23 -0400

    update submission array

--- af_init_guess/scripts/run_af2_initial_guess.sh
+++ af_init_guess/scripts/run_af2_initial_guess.sh
@@ -2,4 +2,4 @@ mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/logs/
 mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/inputs/kras_chunks
 mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/af2_kras
 
-sbatch --array=1-49 run_af2_initial_guess_array.sh
+sbatch --array=0-49 run_af2_initial_guess_array.sh
