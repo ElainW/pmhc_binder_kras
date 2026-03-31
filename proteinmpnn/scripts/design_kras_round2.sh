87514f9af27ffa538be5f74a846f74071a78a41b
Yilan Wang <wangyilan0304@gmail.com>
2026-03-31 18:12:58 -0400

    add scripts to do round 2 proteinmpnn

---
+++ proteinmpnn/scripts/design_kras_round2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem=32g
+#SBATCH --gres=gpu:rtx2080:1
+#SBATCH -c 3
+#SBATCH --output=../slurm/kras_r2.out
+
+# source activate mlfold
+#PYTHON=/n/groups/marks/software/anaconda_o2/envs/proteingym_env/bin/python
+PYTHON="/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym2/model_envs/proteinmpnn/bin/python"
+PROTEINMPNN_SCRIPT_DIR="/n/groups/marks/users/aaron/enzymes/ProteinMPNN"
+folder_with_pdbs="/n/groups/marks/users/aaron/pmhc/rfdiffusion/outputs/kras/partial_r2/filtered"
+input_dir="../input/kras_r2"
+output_dir="../outputs/kras_r2"
+
+mkdir -p $input_dir
+mkdir -p $output_dir
+
+path_for_parsed_chains=$input_dir"/parsed_pdbs.jsonl"
+path_for_assigned_chains=$input_dir"/assigned_pdbs.jsonl"
+
+chains_to_design="A"
+
+${PYTHON} ${PROTEINMPNN_SCRIPT_DIR}/helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
+
+${PYTHON} ${PROTEINMPNN_SCRIPT_DIR}/helper_scripts/assign_fixed_chains.py --input_path=$path_for_parsed_chains --output_path=$path_for_assigned_chains --chain_list "$chains_to_design"
+
+${PYTHON} ${PROTEINMPNN_SCRIPT_DIR}/protein_mpnn_run.py \
+        --jsonl_path $path_for_parsed_chains \
+        --chain_id_jsonl $path_for_assigned_chains \
+        --out_folder $output_dir \
+        --num_seq_per_target 8 \
+        --sampling_temp "0.1" \
+        --seed 31 \
+        --batch_size 1
