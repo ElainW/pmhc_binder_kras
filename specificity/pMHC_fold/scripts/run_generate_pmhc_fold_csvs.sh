d316af290d0564e87409ff098a1a98aa017191b3
Yilan Wang <wangyilan0304@gmail.com>
2026-04-05 19:15:45 -0400

    update csv output dir

--- specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
+++ specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
@@ -6,4 +6,4 @@ python generate_pmhc_fold_csvs.py \
     --pdb_dir /n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/r2/af2_kras/top_pdbs/ \
     --wt_chainB /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/extracted_8I5E.pdb \
     --off_target_pdb_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/r2/off_target_pdbs/ \
-    --out_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/outputs/r2/
+    --out_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/r2/
