63ef1d89924ea1713b88c61586dfb743be224504
Yilan Wang <wangyilan0304@gmail.com>
2026-04-05 18:42:36 -0400

    update script dir and proteinmpnn env

--- specificity/ProteinMPNN_ala/scripts/run_alanine_scan.sh
+++ specificity/ProteinMPNN_ala/scripts/run_alanine_scan.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 cd /n/groups/marks/users/aaron/pmhc/pMHCI_binder_design/software/mpnn/
-PYTHON="/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym2/model_envs/proteinmpnn/bin/python"
+PYTHON="/n/groups/marks/users/aaron/pmhc/envs/proteinmpnn/bin/python"
 folder_with_pdbs="/n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/r2/af2_kras/top_pdbs/"
 pepscan_csv="/n/groups/marks/users/aaron/pmhc/specificity/ProteinMPNN_ala/inputs/pepscan_kras.csv"
+DIR="/n/groups/marks/users/aaron/pmhc/specificity/ProteinMPNN_ala/"
 
-/n/groups/marks/users/aaron/pmhc/envs/dl_binder_design/bin/python generate_mpnn_pepscan_csv.py \
+/n/groups/marks/users/aaron/pmhc/envs/dl_binder_design/bin/python ${DIR}/scripts/generate_mpnn_pepscan_csv.py \
     --pdb_dir ${folder_with_pdbs} \
     --out_csv ${pepscan_csv}
 
-# ${PYTHON} zero_shot_mutation_effect_prediction.py \
-#     --csv_file ${pepscan_csv} \
-#     --folder_with_pdbs ${folder_with_pdbs} \
-#     --output_dir /n/groups/marks/users/aaron/pmhc/specificity/ProteinMPNN_ala/outputs/ \
-#     --use_mt_structure 0 \
-#     --num_seq_per_target 10 \
-#     --batch_size 10 \
-#     --mutant_column mutant \
-#     --mutant_chain_column mutant_chain
+${PYTHON} zero_shot_mutation_effect_prediction.py \
+    --csv_file ${pepscan_csv} \
+    --folder_with_pdbs ${folder_with_pdbs} \
+    --output_dir ${DIR}/outputs/ \
+    --use_mt_structure 0 \
+    --num_seq_per_target 10 \
+    --batch_size 10 \
+    --mutant_column mutant \
+    --mutant_chain_column mutant_chain
