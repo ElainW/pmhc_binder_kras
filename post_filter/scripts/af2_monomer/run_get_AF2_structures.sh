89ac0966f0db6afc2e8f3ba3ea789f70f71e3f84
Yilan Wang <wangyilan0304@gmail.com>
2026-04-07 19:59:31 -0400

    update dirs

--- post_filter/scripts/af2_monomer/run_get_AF2_structures.sh
+++ post_filter/scripts/af2_monomer/run_get_AF2_structures.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-module load alphafold/2.3.1 gcc/9.2.0 cuda/11.2 hh-suite/3.3.0 python/3.10.11
+module load alphafold/2.3.2-020cd6d
 
 # for Domainome, a few jobs took more than 3h, but most finish within 2h, 25G is enough for all
-DIR=Beltran_Lehner_2025 # can use the DMS ID
-export fasta_sequence=/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym2/ColabFold/Beltran_Lehner_2025/beltran_domains_filtered.fa
-export output_dir=$DIR/output
-export max_template_date='2025-04-10' # can put in the date when you run AF2
-mkdir -p $DIR/slurm
-mkdir -p $DIR/fasta
+export fasta_sequence=$1
+export fasta_output_dir=$2
+export af2_output_dir=$3
+export slurm_dir=$4
+export max_template_date=$5 # can put in the date when you run AF2
 
-# sbatch -o $DIR/slurm/AF2-%j.out -e $DIR/slurm/AF2-%j.err --job-name="domainome-AF2" get_AF2_structures.sh ${fasta_sequence} ${max_template_date} ${output_dir}
-python3 split_fasta_submit_af2_jobs.py ${fasta_sequence} ${output_dir} ${max_template_date}
+mkdir -p ${af2_output_dir}
+mkdir -p ${slurm_dir}
+
+if [ ! -f ${fasta_sequence} ]; do
+    echo "No fasta sequence found! Exiting..."
+    exit 1
+fi
+
+python3 split_fasta_submit_af2_jobs.py ${fasta_sequence} ${fasta_output_dir} ${af2_output_dir} ${slurm_dir} ${max_template_date}
