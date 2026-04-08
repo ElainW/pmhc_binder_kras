2880629013a3a7f83b60487bc17dc08ca1d1093d
Yilan Wang <wangyilan0304@gmail.com>
2026-04-07 20:20:31 -0400

    use full path

--- post_filter/scripts/af2_monomer/run.sh
+++ post_filter/scripts/af2_monomer/run.sh
@@ -2,10 +2,7 @@
 module load conda/miniforge3/24.11.3-0
 conda activate /n/groups/marks/users/aaron/pmhc/envs/dl_binder_design
 
-cd /n/groups/marks/users/aaron/pmhc/post_filter/
-mkdir -p inputs/r2/monomer_fasta/
-mkdir -p outputs/r2/af2_monomer/
-mkdir -p slurm/r2/af2_monomer/
+DIR=/n/groups/marks/users/aaron/pmhc/post_filter/
 # 1. Prepare AF2 monomer FASTA
 python scripts/af2_monomer/prepare_af2_monomer_inputs.py \
     --merged_scores /n/groups/marks/users/aaron/pmhc/specificity/analysis/r2/merged_scores_ranked.tsv \
@@ -14,7 +11,7 @@ python scripts/af2_monomer/prepare_af2_monomer_inputs.py \
     --out_dir inputs/r2/af2_monomer/af2_monomer_inputs.fasta
 
 # 2. Split the fasta and submit sbatch jobs
-bash scripts/af2_monomer/run_get_AF2_structures.sh inputs/r2/af2_monomer/af2_monomer_inputs.fasta inputs/r2/af2_monomer/ outputs/r2/af2_monomer/ slurm/r2/af2_monomer/ '2026-04-07'
+bash run_get_AF2_structures.sh $DIR/inputs/r2/monomer_fasta/af2_monomer_inputs.fasta $DIR/inputs/r2/monomer_fasta/ $DIR/outputs/r2/af2_monomer/ $DIR/slurm/r2/af2_monomer/ '2026-04-07'
 
 # # 4. Collect the scores after the sbatch job finishes
 # python collect_monomer_scores.py \
