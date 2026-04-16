02a20a664266d2532d188d3eaa94efdb6b0dc666
Yilan Wang <wangyilan0304@gmail.com>
2026-04-16 15:15:56 -0400

    update cmd for collect score and remove fasta making

--- post_filter/scripts/af2_monomer/run.sh
+++ post_filter/scripts/af2_monomer/run.sh
@@ -3,17 +3,17 @@ module load conda/miniforge3/24.11.3-0
 conda activate /n/groups/marks/users/aaron/pmhc/envs/dl_binder_design
 
 DIR=/n/groups/marks/users/aaron/pmhc/post_filter/
-# 1. Prepare AF2 monomer FASTA
-python scripts/af2_monomer/prepare_af2_monomer_inputs.py \
-    --merged_scores /n/groups/marks/users/aaron/pmhc/specificity/analysis/r2/merged_scores_ranked.tsv \
-    --pdb_dir /n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/r2/af2_kras/top_pdbs/ \
-    --author_csv inputs/science.adv0185_data_s1.csv \
-    --out_dir inputs/r2/af2_monomer/af2_monomer_inputs.fasta
+# 1. Prepare AF2 monomer FASTA --> did this manually
+# python scripts/af2_monomer/prepare_af2_monomer_inputs.py \
+#     --merged_scores /n/groups/marks/users/aaron/pmhc/specificity/analysis/r2/merged_scores_ranked.tsv \
+#     --pdb_dir /n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/r2/af2_kras/top_pdbs/ \
+#     --author_csv inputs/science.adv0185_data_s1.csv \
+#     --out_dir inputs/r2/af2_monomer/af2_monomer_inputs.fasta
 
 # 2. Split the fasta and submit sbatch jobs
-bash run_get_AF2_structures.sh $DIR/inputs/r2/monomer_fasta/af2_monomer_inputs.fasta $DIR/inputs/r2/monomer_fasta/ $DIR/outputs/r2/af2_monomer/ $DIR/slurm/r2/af2_monomer/ '2026-04-07'
+# bash run_get_AF2_structures.sh $DIR/inputs/r2/monomer_fasta/af2_monomer_inputs.fasta $DIR/inputs/r2/monomer_fasta/ $DIR/outputs/r2/af2_monomer/ $DIR/slurm/r2/af2_monomer/ '2026-04-07'
 
-# # 4. Collect the scores after the sbatch job finishes
-# python collect_monomer_scores.py \
-#     --monomer_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/ \
-#     --out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/
+# 3. Collect the scores after the sbatch job finishes
+python collect_monomer_scores.py \
+    --monomer_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/ \
+    --out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/
