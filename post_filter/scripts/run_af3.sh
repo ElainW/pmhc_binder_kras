c68e63eec429e7746cece4fa902e3df24d228666
Yilan Wang <wangyilan0304@gmail.com>
2026-04-23 15:16:55 -0400

    convert fastrelax to sbatch and add author design run cmds

--- post_filter/scripts/run_af3.sh
+++ post_filter/scripts/run_af3.sh
@@ -1,11 +1,58 @@
 module load conda/miniforge3/24.11.3-0
 conda activate /n/groups/marks/users/aaron/pmhc/envs/dl_binder_design
 
+# for authors' designs
+slurm_dir=/n/groups/marks/users/aaron/pmhc/post_filter/slurm
 # step 0: run fastrelax
+sbatch -p short -c 1 -t 0-2:00 --mem=10G -o ${slurm_dir}/af3-nomsa-design-%j.out -e ${slurm_dir}/af3-nomsa-design-%j.err -J af3-nomsa-design --wrap="
+python fastrelax_designs_af3.py \
+    --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
+    --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/ \
+    --n_repeats   3"
+
+sbatch -p short -c 1 -t 0-2:00 --mem=10G -o ${slurm_dir}/af3-author-%j.out -e ${slurm_dir}/af3-author-%j.err -J af3-author-design --wrap="
+python fastrelax_designs_af3.py \
+    --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
+    --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/ \
+    --n_repeats   3"
+
+# step 1: calculate stats
+python af3_design_stats.py \
+    --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
+    --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/relaxed_pdbs/ \
+    --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv
+
+python af3_design_stats.py \
+    --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
+    --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/relaxed_pdbs/ \
+    --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv
+
+# step 2: visualize stats
+python plot_author_stats.py \
+    --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/af3_design_stats.tsv \
+    --fastrelax_tsv /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/fastrelax_af3_scores.tsv \
+    --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
+    --xlsx         /n/groups/marks/users/aaron/pmhc/post_filter/inputs/science.adv0185_data_s1.csv \
+    --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_authors.tsv \
+    --out_dir      /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_plots/
+
+python plot_author_stats.py \
+    --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/af3_design_stats.tsv \
+    --fastrelax_tsv /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/fastrelax_af3_scores.tsv \
+    --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
+    --xlsx         /n/groups/marks/users/aaron/pmhc/post_filter/inputs/science.adv0185_data_s1.csv \
+    --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_authors.tsv \
+    --out_dir      /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa_plots/
+
+
+# step 0: run fastrelax
+sbatch -p priority -c 1 -t 0-12:00 --mem=10G -o ${slurm_dir}/af3-r2-%j.out -e ${slurm_dir}/af3-r2-%j.err -J af3-r2-design --wrap="
 python fastrelax_designs_af3.py \
     --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3/ \
     --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/ \
-    --n_repeats   3
+    --n_repeats   3"
 
 # step 1: calculate stats
 # make design_epitopes.csv
