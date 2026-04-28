e07a17322f8288c9a33f9737dae54bacded2c347
Yilan Wang <wangyilan0304@gmail.com>
2026-04-28 17:06:54 -0400

    add cmds to run af3 stats calculation and visualization for my designs

--- post_filter/scripts/run_af3.sh
+++ post_filter/scripts/run_af3.sh
@@ -4,16 +4,18 @@ conda activate /n/groups/marks/users/aaron/pmhc/envs/dl_binder_design
 # for authors' designs
 slurm_dir=/n/groups/marks/users/aaron/pmhc/post_filter/slurm
 # step 0: run fastrelax
-sbatch -p short -c 1 -t 0-2:00 --mem=10G -o ${slurm_dir}/af3-nomsa-design-%j.out -e ${slurm_dir}/af3-nomsa-design-%j.err -J af3-nomsa-design --wrap="
+sbatch -p short -c 1 -t 0-2:00 --mem=2G -o ${slurm_dir}/af3-nomsa-design-%j.out -e ${slurm_dir}/af3-nomsa-design-%j.err -J af3-nomsa-design --wrap="
 python fastrelax_designs_af3.py \
     --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
     --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/ \
+    --dalphaball /n/groups/marks/users/aaron/pmhc/post_filter/scripts/DAlphaBall.gcc \
     --n_repeats   3"
 
-sbatch -p short -c 1 -t 0-2:00 --mem=10G -o ${slurm_dir}/af3-author-%j.out -e ${slurm_dir}/af3-author-%j.err -J af3-author-design --wrap="
+sbatch -p short -c 1 -t 0-2:00 --mem=2G -o ${slurm_dir}/af3-author-%j.out -e ${slurm_dir}/af3-author-%j.err -J af3-author-design --wrap="
 python fastrelax_designs_af3.py \
     --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
     --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/ \
+    --dalphaball /n/groups/marks/users/aaron/pmhc/post_filter/scripts/DAlphaBall.gcc \
     --n_repeats   3"
 
 # step 1: calculate stats
@@ -21,18 +23,19 @@ python af3_design_stats.py \
     --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
     --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/relaxed_pdbs/ \
     --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/ \
-    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
+    --fastrelax_tsv   /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/fastrelax_af3_scores.tsv
 
 python af3_design_stats.py \
     --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
     --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/relaxed_pdbs/ \
     --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/ \
-    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
+    --fastrelax_tsv   /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/fastrelax_af3_scores.tsv
 
 # step 2: visualize stats
 python plot_author_stats.py \
     --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3/af3_design_stats.tsv \
-    --fastrelax_tsv /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3/fastrelax_af3_scores.tsv \
     --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
     --xlsx         /n/groups/marks/users/aaron/pmhc/post_filter/inputs/science.adv0185_data_s1.csv \
     --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_authors.tsv \
@@ -40,7 +43,6 @@ python plot_author_stats.py \
 
 python plot_author_stats.py \
     --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/af3_nomsa/af3_design_stats.tsv \
-    --fastrelax_tsv /n/groups/marks/users/aaron/pmhc/post_filter/outputs/author_design_stats/fastrelax_af3_nomsa/fastrelax_af3_scores.tsv \
     --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/design_epitopes.csv \
     --xlsx         /n/groups/marks/users/aaron/pmhc/post_filter/inputs/science.adv0185_data_s1.csv \
     --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_authors.tsv \
@@ -48,33 +50,48 @@ python plot_author_stats.py \
 
 
 # step 0: run fastrelax
-sbatch -p priority -c 1 -t 0-12:00 --mem=10G -o ${slurm_dir}/af3-r2-%j.out -e ${slurm_dir}/af3-r2-%j.err -J af3-r2-design --wrap="
-python fastrelax_designs_af3.py \
-    --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3/ \
-    --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/ \
-    --n_repeats   3"
+sbatch -p priority -c 1 -t 0-12:00 --mem=3G -o ${slurm_dir}/af3-r2-%j.out -e ${slurm_dir}/af3-r2-%j.err -J af3-r2-design --wrap="
+ python fastrelax_designs_af3.py \
+     --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3/ \
+     --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/ \
+     --dalphaball /n/groups/marks/users/aaron/pmhc/post_filter/scripts/DAlphaBall.gcc \
+     --n_repeats   3"
 
-# step 1: calculate stats
-# make design_epitopes.csv
-python write_design_epitope.py \
-    --fasta /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/binder_pMHC_full.fasta \
-    --epitope_resnum 5 \
-    --epitope_notes "G12D so 5th residue in the peptide"
-    --hla A*11:01 \
-    --target_name KRAS \
-    --output_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv
+sbatch -p priority -c 1 -t 0-12:00 --mem=3G -o ${slurm_dir}/af3-r2-nomsa-%j.out -e ${slurm_dir}/af3-r2-nomsa-%j.err -J af3-r2-nomsa-design --wrap="
+ python fastrelax_designs_af3.py \
+     --af3_out_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_nomsa/ \
+     --out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3_nomsa/ \
+     --dalphaball /n/groups/marks/users/aaron/pmhc/post_filter/scripts/DAlphaBall.gcc \
+     --n_repeats   3"
+
+# # step 1: calculate stats
+# # make design_epitopes.csv
+# python write_design_epitope.py \
+#     --fasta /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/binder_pMHC_full.fasta \
+#     --epitope_resnum 5 \
+#     --epitope_notes "G12D so 5th residue in the peptide"
+#     --hla A*11:01 \
+#     --target_name KRAS \
+#     --output_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv
 
 python af3_design_stats.py \
     --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3/ \
     --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/relaxed_pdbs/ \
-    --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/ \
-    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv
+    --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3/ \
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv \
+    --fastrelax_tsv   /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/relaxed_pdbs/fastrelax_af3_scores.tsv
 
-# step 2: visualize stats
-python plot_design_stats.py \
-    --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_design_stats.tsv \
-    --fastrelax_tsv /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/fastrelax_af3_scores.tsv \
-    --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv \
-    --sequences /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/monomer_fasta/af2_monomer_sequences.csv \
-    --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_your_designs.tsv \
-    --out_dir      /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_plots/
+python af3_design_stats.py \
+    --af3_out_dir     /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_nomsa/ \
+    --relaxed_pdb_dir /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3_nomsa/relaxed_pdbs/ \
+    --out_dir         /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_nomsa/ \
+    --targets_csv     /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv \
+    --fastrelax_tsv   /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/fastrelax_af3/relaxed_pdbs/fastrelax_af3_scores.tsv
+
+# # step 2: visualize stats
+# python plot_design_stats.py \
+#     --stats_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_design_stats.tsv \
+#     --epitopes_csv /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/design_epitopes.csv \
+#     --sequences /n/groups/marks/users/aaron/pmhc/post_filter/inputs/r2/monomer_fasta/af2_monomer_sequences.csv \
+#     --plddt_tsv    /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af2_monomer/stats/monomer_scores_your_designs.tsv \
+#     --out_dir      /n/groups/marks/users/aaron/pmhc/post_filter/outputs/r2/af3_plots/
