cb24349a5e96a6caf415e5fa5e4ec6e329d5c93f
Yilan Wang <wangyilan0304@gmail.com>
2026-05-06 23:37:38 -0400

    add cmd_runner.py for af2 monomer runs

--- post_filter/scripts/af2_monomer/cmd_runner.py
+++ post_filter/scripts/af2_monomer/cmd_runner.py
@@ -4,30 +4,30 @@ from subprocess import *
 
 
 def run_cmd_small_output(cmd):
-	print(f"Running command: {cmd}\n")
-	Popen(cmd, shell=True, stdout=PIPE).communicate()
+    print(f"Running command: {cmd}\n")
+    Popen(cmd, shell=True, stdout=PIPE).communicate()
 
 def run_cmd_to_file(cmd, sf_out):
-	print(f"Running command with output:\n{cmd}\nto {sf_out}\n")
-	sf_err=sf_out+".err"
-	errcode=None
-	with open(sf_out, "w") as f:
-		p = Popen(cmd, shell=True, stdout=f, stderr=None)
-		p.communicate()
-	if os.path.isfile(sf_err):
-		os.remove(sf_err)
-	return errcode
+    print(f"Running command with output:\n{cmd}\nto {sf_out}\n")
+    sf_err=sf_out+".err"
+    errcode=None
+    with open(sf_out, "w") as f:
+        p = Popen(cmd, shell=True, stdout=f, stderr=None)
+        p.communicate()
+    if os.path.isfile(sf_err):
+        os.remove(sf_err)
+    return errcode
 
 def run_cmd_small_output_with_blocking(cmd):
-	print(f"Running command: {cmd}\n")
-	call(cmd, shell=True, stdout=None)
+    print(f"Running command: {cmd}\n")
+    call(cmd, shell=True, stdout=None)
 
 def run_cmd_to_file_with_blocking(cmd, sf_out):
-	print(f"Running command with output:\n{cmd} > {sf_out}\nWait until this process is finished.")
-	sf_err=sf_out+".err"
-	errcode=None
-	with open(sf_out, "w") as f:
-		p = call(cmd, shell=True, stdout=f, stderr=None)
-	if os.path.isfile(sf_err):
-		os.remove(sf_err)
-	return errcode
+    print(f"Running command with output:\n{cmd} > {sf_out}\nWait until this process is finished.")
+    sf_err=sf_out+".err"
+    errcode=None
+    with open(sf_out, "w") as f:
+        p = call(cmd, shell=True, stdout=f, stderr=None)
+    if os.path.isfile(sf_err):
+        os.remove(sf_err)
+    return errcode
