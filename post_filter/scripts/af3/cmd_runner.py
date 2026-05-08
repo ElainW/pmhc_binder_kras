3a6fbccdd90be20691a4d54bf8603aa3150cf7ac
Yilan Wang <wangyilan0304@gmail.com>
2026-05-08 02:31:23 -0400

    type change

--- post_filter/scripts/af3/cmd_runner.py
+++ post_filter/scripts/af3/cmd_runner.py
@@ -1,1 +1,33 @@
-/n/groups/marks/users/aaron/pmhc/post_filter/scripts/af2_monomer/cmd_runner.py
+#!/bin/python3
+import os
+from subprocess import *
+
+
+def run_cmd_small_output(cmd):
+    print(f"Running command: {cmd}\n")
+    Popen(cmd, shell=True, stdout=PIPE).communicate()
+
+def run_cmd_to_file(cmd, sf_out):
+    print(f"Running command with output:\n{cmd}\nto {sf_out}\n")
+    sf_err=sf_out+".err"
+    errcode=None
+    with open(sf_out, "w") as f:
+        p = Popen(cmd, shell=True, stdout=f, stderr=None)
+        p.communicate()
+    if os.path.isfile(sf_err):
+        os.remove(sf_err)
+    return errcode
+
+def run_cmd_small_output_with_blocking(cmd):
+    print(f"Running command: {cmd}\n")
+    call(cmd, shell=True, stdout=None)
+
+def run_cmd_to_file_with_blocking(cmd, sf_out):
+    print(f"Running command with output:\n{cmd} > {sf_out}\nWait until this process is finished.")
+    sf_err=sf_out+".err"
+    errcode=None
+    with open(sf_out, "w") as f:
+        p = call(cmd, shell=True, stdout=f, stderr=None)
+    if os.path.isfile(sf_err):
+        os.remove(sf_err)
+    return errcode
