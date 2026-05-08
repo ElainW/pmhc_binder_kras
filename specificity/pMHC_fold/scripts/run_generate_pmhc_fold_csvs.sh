c21d3907558351da5380fa695c6bfe595c51e7a6
Yilan Wang <wangyilan0304@gmail.com>
2026-05-08 00:10:29 -0400

    add r2.5 cmd

--- specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
+++ specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
@@ -7,3 +7,9 @@ python generate_pmhc_fold_csvs.py \
     --wt_chainB /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/extracted_8I5E.pdb \
     --off_target_pdb_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/r2/off_target_pdbs/ \
     --out_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/r2/
+
+python generate_pmhc_fold_csvs.py \
+    --pdb_dir /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/outputs/r2.5/af2_kras/top_pdbs/ \
+    --wt_chainB /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/extracted_8I5E.pdb \
+    --off_target_pdb_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r2.5/off_target_pdbs/ \
+    --out_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r2.5/
