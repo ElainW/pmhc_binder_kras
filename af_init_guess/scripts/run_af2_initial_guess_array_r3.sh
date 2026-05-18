0566d34bf64d1ab372a30484a26d2d4047e7dd78
Yilan Wang <wangyilan0304@gmail.com>
2026-05-18 14:45:16 -0400

    fix typo

--- af_init_guess/scripts/run_af2_initial_guess_array_r3.sh
+++ af_init_guess/scripts/run_af2_initial_guess_array_r3.sh
@@ -42,7 +42,7 @@ fi
 
 # --- Paths ---
 DL_BINDER_DESIGN_DIR="/n/groups/marks/users/aaron/pmhc/dl_binder_design"
-CHUNK_DIR="/n/groups/marks/users/aaron/pmhc/af_init_guess/inputs/r3/kras_chunks"
+CHUNK_DIR="/n/groups/marks/users/aaron/pmhc_cp/af_init_guess/inputs/r3/kras_chunks"
 OUT_DIR="/n/groups/marks/users/aaron/pmhc_cp/af_init_guess/outputs/r3/af2_kras"
 
 # --- Per-job inputs/outputs based on array index ---
