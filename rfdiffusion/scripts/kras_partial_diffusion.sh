ef300b826570617c55d51944725156afd8bf2c29
Yilan Wang <wangyilan0304@gmail.com>
2026-05-17 21:08:34 -0400

    delete the file

--- rfdiffusion/scripts/kras_partial_diffusion.sh
+++
@@ -1,6 +0,0 @@
-# Make executable
-chmod +x sbatch_partdiff_tier*.sh
-
-sbatch sbatch_partdiff_tier1.sh   # 7 backbones × 100 = 700 trajectories
-sbatch sbatch_partdiff_tier2.sh   # 8 backbones × 50  = 400 trajectories
-sbatch sbatch_partdiff_tier3.sh   # 10 backbones × 20  = 200 trajectories
