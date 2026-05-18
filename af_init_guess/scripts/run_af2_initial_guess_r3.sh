bb7bd7971256b3142a252733d80aad2d1f72e13c
Yilan Wang <wangyilan0304@gmail.com>
2026-05-18 13:22:52 -0400

    update directories

--- af_init_guess/scripts/run_af2_initial_guess_r3.sh
+++ af_init_guess/scripts/run_af2_initial_guess_r3.sh
@@ -1,5 +1,5 @@
-mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/logs/
-mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/inputs/r3/kras_chunks
-mkdir -p /n/groups/marks/users/aaron/pmhc/af_init_guess/outputs/r3/af2_kras
+mkdir -p /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/logs/
+mkdir -p /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/inputs/r3/kras_chunks
+mkdir -p /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/outputs/r3/af2_kras
 
 sbatch --array=0-13 run_af2_initial_guess_array_r3.sh
