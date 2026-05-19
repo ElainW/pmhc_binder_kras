41e334d002e558a4645e9872ea99c854c8459605
Yilan Wang <wangyilan0304@gmail.com>
2026-05-19 14:20:40 -0400

    add cmd to set up round 3 pMHC fold screen

--- specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
+++ specificity/pMHC_fold/scripts/run_generate_pmhc_fold_csvs.sh
@@ -13,3 +13,9 @@ python generate_pmhc_fold_csvs.py \
     --wt_chainB /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/extracted_8I5E.pdb \
     --off_target_pdb_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r2.5/off_target_pdbs/ \
     --out_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r2.5/
+
+python generate_pmhc_fold_csvs.py \
+    --pdb_dir /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/outputs/r3/af2_kras/top_pdbs/ \
+    --wt_chainB /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/extracted_8I5E.pdb \
+    --off_target_pdb_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r3/off_target_pdbs/ \
+    --out_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r3/
