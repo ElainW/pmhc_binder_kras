c8224413ab5fdd1a7c3a730d0412608fe120e712
Yilan Wang <wangyilan0304@gmail.com>
2026-05-19 14:32:48 -0400

    move commands to run pMHC fold to a shell script

--- specificity/pMHC_fold/scripts/batch_pMHC_fold_o2.py
+++ specificity/pMHC_fold/scripts/batch_pMHC_fold_o2.py
@@ -16,16 +16,6 @@ On-target example:
         --structs_per_job 20 \
         --t 01:30:00
 
-    python batch_pMHC_fold_o2.py \
-        --prefix pmhc_fold_on \
-        --round r2.5 \
-        --script /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/scripts/pmhc_fold.py \
-        --alleles "A*11:01" \
-        --peptides VVGADGVGK \
-        --minib_dir /n/groups/marks/users/aaron/pmhc_cp/af_init_guess/outputs/r2.5/af2_kras/top_pdbs/ \
-        --structs_per_job 20 \
-        --t 01:30:00
-
 Off-target example:
     python batch_pMHC_fold_o2.py \
         --prefix pmhc_fold_off \
@@ -34,16 +24,6 @@ Off-target example:
         --alleles "A*11:01" \
         --peptides VVGAGGVGK \
         --minib_dir /n/groups/marks/users/aaron/pmhc/specificity/pMHC_fold/inputs/r2/off_target_pdbs/ \
-        --structs_per_job 20 \
-        --t 01:30:00
-
-    python batch_pMHC_fold_o2.py \
-        --prefix pmhc_fold_off \
-        --round r2.5 \
-        --script /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/scripts/pmhc_fold.py \
-        --alleles "A*11:01" \
-        --peptides VVGAGGVGK \
-        --minib_dir /n/groups/marks/users/aaron/pmhc_cp/specificity/pMHC_fold/inputs/r2.5/off_target_pdbs/ \
         --structs_per_job 20 \
         --t 01:30:00
 """
